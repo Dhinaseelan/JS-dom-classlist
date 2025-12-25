3661b7850069350922ee14690becf820aa838714
Dhinaseelan <dhinaseeland@gmail.com>
2025-12-25 21:43:06 +0530

    fix:stoppropagation and stopimmetediatepropagation,capturing

--- bubblincapturing.js
+++ bubblincapturing.js
@@ -8,4 +8,28 @@ formel.addEventListener("click",(event)=>{
 
 divel.addEventListener("click",()=>{console.log(`div`)})
 pel.addEventListener("click",()=>{console.log("p ")})
+//fix-----------------------------------fix-----------------------------fix--------------------------fix
+let formel=document.querySelector(".formel");
+let divel=document.querySelector(".divel");
+let pel=document.querySelector(".pel");
+formel.addEventListener("click",(event)=>{
+    console.log(`where form ${event.target.tagName} this tag name ${formel.tagName}`)
+})
 
+divel.addEventListener("click",(event)=>{console.log(`div`)
+//adding stop propagation 
+event.stopPropagation();
+//this stop next bubbling the same name this is last bubbling
+event.stopImmediatePropagation();
+})
+pel.addEventListener("click",()=>{console.log("p ")})
+divel.addEventListener("click",()=>{console.log("last div")},)
+
+formel.addEventListener("click",()=>{console.log("form")},{capture:true})
+//for catch the capturing phase &  bubbling
+for(let pro of document.querySelectorAll("form, form *")){
+    //capturing
+    pro.addEventListener("click",()=>{console.log(`capturing phrase: ${pro.tagName} `)},{capture:true})j
+    //bubbling phase
+    pro.addEventListener("click",()=>{console.log(`bubbling phase:${pro.tagName}`)})
+}
