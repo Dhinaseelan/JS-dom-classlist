1a016151128cfce37c77e81d6d289fff790332a1
Dhinaseelan <dhinaseeland@gmail.com>
2025-12-29 15:43:16 +0530

    fead:keyboardevent

---
+++ keyboardevent.js
@@ -0,0 +1,20 @@
+document.addEventListener("keyup",(event)=>{
+    console.log(event.target);
+    switch(event.key){
+        case "ArrowUp":
+            alert(" you clicked the Uparrow");
+            break;
+        case "ArrowDown":
+            alert("you clicked down arrow");
+            break;
+        case "ArrowLeft":
+            alert("you cliked left arrow");
+            break;
+        case "ArrowRight":
+            alert("you clicked right arrow");
+            break;
+        default:
+            alert("you clicked wrong button");
+            break;
+    }
+})
