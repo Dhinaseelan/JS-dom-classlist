b879a0d9beb98a225b94ae9bb2dd8879c07b4ab3
Dhinaseelan <dhinaseeland@gmail.com>
2026-01-01 23:07:29 +0530

    fix:Form Submit and FormData Event

--- form-value-accessing/script.js
+++ form-value-accessing/script.js
@@ -13,8 +13,31 @@ const formval=document.forms.Feedback;// accessing forms by form name
 // const email=formval.elements.email;
 // const discip=formval.elements.discription;
 // console.log(fullname.placeholder);
-console.log("kathir")
-console.log(formval);
+// console.log("kathir")
+// console.log(formval);
 //accessing elements by object destrucing
-const {name,type,email,discription}=formval.elements;
-console.log(name,type,email,discription);
+// const {name,type,email,discription}=formval.elements;
+// console.log(name,type,email,discription);
+const handleform=(event)=>{
+    event.preventDefault()
+    const formdata=new FormData(formval);
+
+}
+const handleformdata=(e)=>{
+    const formData=e.formData;
+    console.log(formData);
+    //you can adding new key and value to formdata
+    formData.append("myname","kathir");
+    //this method delete a particular key and value
+    formData.delete("name");
+    //the entries method is you do get all key and value
+   console.log( [...formData.entries()]);
+   //has method is used to a particular data is present or not it present it returns true it not present it return false
+    console.log(formData.has("name"));
+    //it returns the entier keys of form data
+    console.log([...formData.key()]);
+    //it return the entier values of the formdata
+    console.log([...formData.values()])
+}
+formval.addEventListener("submit",handleform);
+formval.addEventListener("formdata",handleformdata);
